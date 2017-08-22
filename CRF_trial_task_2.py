e2780d0af76802b9a3e7e3cd906d9efdf336ddb4
Srinivasan <srinivasan.ma2009@hotmail.com>
2017-08-22 03:25:58 +0200

    Task 1 in progress

--- CRF_trial_task_2.py
+++ CRF_trial_task_2.py
@@ -1,40 +1,34 @@
 import os
 import sys
 import matplotlib.pyplot as plt
+import time
+import pprint as pp
 
 plt.style.use('ggplot')
-from itertools import chain
-
-import nltk
-import sklearn
-import scipy.stats
-from sklearn.metrics import make_scorer
-from sklearn.cross_validation import cross_val_score
-from sklearn.grid_search import RandomizedSearchCV
 
 import sklearn_crfsuite
-from sklearn_crfsuite import scorers
 from sklearn_crfsuite import metrics
-import pprint as pp
 
 
 # Manage imports above this line
+
 def word2features(sent, i):
     word = sent[i][0]
     postag = sent[i][1]
 
     features = {
         # 'bias': 1.0,
+        'word': word,
         'word.lower()': word.lower(),
         'word[-3:]': word[-3:],
-        # 'word[-2:]': word[-2:],
+        'word[-2:]': word[-2:],
         'word.isupper()': word.isupper(),
         'word.istitle()': word.istitle(),
         'word.isdigit()': word.isdigit(),
         'postag': postag,
-        # 'postag[:2]': postag[:2],
+        'postag[:2]': postag[:2],
     }
-    """
+
     if i > 0:
         word1 = sent[i - 1][0]
         postag1 = sent[i - 1][1]
@@ -60,7 +54,7 @@ def word2features(sent, i):
         })
     else:
         features['EOS'] = True
-    """
+
     return features
 
 
@@ -69,19 +63,24 @@ def sent2features(sent):
 
 
 def sent2labels(sent):
-    return [label for token, postag, onehot, label in sent]
+    # return [label for token, postag, onehot, label in sent]
+    return [label for token, postag, label in sent]
 
 
 def sent2tokens(sent):
-    return [token for token, postag, onehot, label in sent]
+    # return [token for token, postag, onehot, label in sent]
+    return [token for token, postag, label in sent]
 
 
 def main():
     # Build training data from training files
-    txtcbn_file_folder = "MalwareTextDB-1.0/data/ann+brown/"
-    files = [txtcbn_file_folder + filename.strip() for filename in os.listdir(txtcbn_file_folder)]
+    # txtcbn_file_folder = "MalwareTextDB-1.0/data/ann+brown/"
+    token_file_folder = "MalwareTextDB-1.0/data/tokenized/"
+    # files = [txtcbn_file_folder + filename.strip() for filename in os.listdir(txtcbn_file_folder)]
+    files = [token_file_folder + filename.strip() for filename in os.listdir(token_file_folder)]
     training_data = []
-    print("Found ", len(files), " .txtcbn files with training data")
+    # print("Found ", len(files), " .txtcbn files with training data")
+    print("Found ", len(files), " .token files with training data")
     training_sentences = []
     for file in files:
         training_data.append(("#", file))
@@ -94,12 +93,14 @@ def main():
                 continue
             training_data.append((entries.strip().split(" ")))
             temp.append((entries.strip().split(" ")))
-            if (temp[-1][0] == "." and temp[-1][3] == "O"):
+            # if (temp[-1][0] == "." and temp[-1][3] == "O"):
+            if (temp[-1][0] == "." and temp[-1][2] == "O"):
                 training_sentences.append(temp)
                 temp = []
     print(len(training_data), " samples in training data")
     print(len(training_sentences), " sentences ")
-    training_sentences = [[x for x in y if len(x) == 4] for y in training_sentences]
+    # training_sentences = [[x for x in y if len(x) == 4] for y in training_sentences]
+    training_sentences = [[x for x in y if len(x) == 3] for y in training_sentences]
 
     # Write training data, for visual purposes
     with open("Tokens.tsv", "w", encoding="UTF-8") as tsvout:
@@ -122,18 +123,20 @@ def main():
     Y_test = [sent2labels(sentence) for sentence in training_sentences[int(len(training_sentences) * .80):]]
 
     # Build CRF and fit train data
+    print("Fit training data")
+    start_time = time.time()
     crf = sklearn_crfsuite.CRF(
         algorithm='lbfgs',
         c1=0.1,
         c2=0.1,
         max_iterations=100,
-        all_possible_transitions=True
-    )
+        all_possible_transitions=True,
+    )  # verbose=True
     crf.fit(X_train, Y_train)
 
     # Evaluation
     labels = list(crf.classes_)
-    #labels.remove('O')
+    labels.remove('O')
     print(labels)
     y_pred = crf.predict(X_test)
     metrics.flat_f1_score(Y_test, y_pred,
@@ -145,8 +148,17 @@ def main():
     print(metrics.flat_classification_report(
         Y_test, y_pred, labels=sorted_labels, digits=3
     ))
+    print("Running time : ", time.time() - start_time)
 
-    return 0  # END OF MAIN
+    results = crf.predict(X_test)
+    print("Writing results to file")
+    outfile = open("Predict_out.txt", "w", encoding="UTF-8")
+    for x, y in zip([[x[0] for x in y] for y in training_sentences[int(len(training_sentences) * .80):]], results):
+        for x1, y1 in zip(x, y):
+            outfile.write(x1 + "\t" + y1 + "\n")
+    outfile.close()
+
+    return 0  # End of main
 
 
 if __name__ == '__main__':
---
+++ Task1.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import pprint as pp
+import re
+from nltk import ngrams
+from nltk.corpus import stopwords
+
+# Manage imports above this line
+
+def main():
+    # Read all tokens from Task1and2-input file and build sentences
+    test1_input = "SemEval_input/Task1and2-input"
+    with open(test1_input, encoding="UTF-8") as input_file:
+        entries = [x.strip() for x in input_file.readlines()]
+    print(len(entries), " tokens")
+    sentences = []
+    buffer = []
+    for i, token in enumerate(entries):
+        if (token == "." and entries[i + 1] == ""):
+            buffer.append(token)
+            sentences.append(buffer)
+            buffer = []
+        else:
+            buffer.append(token)
+    print(len(sentences), " sentences")
+
+    # Read Annotation labels file and retrieve the key phrases
+    # and then process them into keywords
+    with open("MalwareTextDB-1.0/annotation_guidelines/Attribute Labels.csv") as labels_file:
+        entries = [x.strip().split(",") for x in labels_file.readlines()]
+        labels_file.close()
+    keyphrases = []
+    stopword = stopwords.words("english")
+    for rows in entries[1:]:
+        keyphrases.append(rows[-1].strip())
+    keyphrases = [x for x in keyphrases if x != ""]
+    keywords = []
+    for entry in keyphrases:
+        keywords.extend([x for x in re.findall(r"[\w ]*", entry) if x != ""])
+    keywords = set(keywords)
+    print(len(keywords), " keywords")
+
+    # For each sentence build ngrams of multiple order and
+    # check for keyword match
+    for i,sentence in enumerate(sentences):
+        sentence = [x.lower() for x in sentence]
+        tokens = []
+        sentence_as_string = " ".join(sentence)
+        print(sentence_as_string)
+        tokens.extend(list(ngrams(sentence_as_string.split(),n=1)))
+        tokens.extend(list(ngrams(sentence_as_string.split(),n=2)))
+        tokens.extend(list(ngrams(sentence_as_string.split(),n=3)))
+        tokens = set([" ".join(x) for x in tokens if x not in stopword])
+        for ngram in tokens:
+            if(ngram in keywords):
+                print(i,ngram)
+                pass
+
+
+    return 0  # End of main
+
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
